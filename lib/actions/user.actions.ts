dc31518836f22d5ea827621a41304b60a18b4e9c
prashant <prashantkr.msh@gmail.com>
2024-05-06 00:37:25 +0530

    initial commit

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,8 +2,8 @@
 
 import { revalidatePath } from "next/cache";
 
-import User from "../database/models/user.model";
-import { connectToDatabase } from "../database/mongoose";
+// import User from "../database/models/user.model";
+import { connectToDatabase } from "../database/Mongoose";
 import { handleError } from "../utils";
 
 // CREATE
